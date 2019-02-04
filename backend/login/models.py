7d65f351aa4d58f8cfc4d68f17fa26fffba5e40c
Md. Moshfiqur Rahman Rony <moshfiqrony@gmail.com>
2019-02-04 11:19:12 +0600

    login model added

--- backend/login/models.py
+++ backend/login/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class UserInfo(models.Model):
     id = models.IntegerField(primary_key=True)
-    username = models.CharField(max_length=80)
+    username = models.CharField(max_length=80, unique=True)
     password = models.CharField(max_length=100)
-    email = models.CharField(max_length=120)
+    email = models.CharField(max_length=120, unique=True)
 
