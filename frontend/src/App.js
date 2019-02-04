79ffcb7a0a779730e2834d85387e99f64880e318
Md. Moshfiqur Rahman Rony <moshfiqrony@gmail.com>
2019-02-04 14:42:36 +0600

    login added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import Form from './component/Form'
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Form/>
       </div>
     );
   }
---
+++ frontend/src/component/Form.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import axios from 'axios'
+
+
+class Form extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            username: '',
+            email: '',
+            password: '',
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(e){
+
+        const {name, value} = e.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit(e){
+        e.preventDefault()
+        console.log(this.state)
+        const username = e.target.elements.username.value
+        const password = e.target.elements.password.value
+        const email = e.target.elements.email.value
+        axios.post('http://127.0.0.1:8000/getbyuser/',
+            {
+                username: username,
+                password: password,
+                email: email
+            })
+            .then(response => console.log(response))
+
+    }
+
+
+    render() {
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type='text'
+                        name='username'
+                        placeholder='Enter Username *'
+                        onChange={this.handleChange}
+                        value={this.state.username}
+                    />
+                    <input
+                        type='email'
+                        name='email'
+                        placeholder='Enter Email *'
+                        onChange={this.handleChange}
+                        value={this.state.email}
+                    />
+                    <input
+                        type='password'
+                        name='password'
+                        placeholder='Enter Password *'
+                        onChange={this.handleChange}
+                        value={this.state.password}
+                    />
+                    <input
+                        type='submit'
+                        name='submit'
+                        value='Register'
+                    />
+                </form>
+                <div>
+
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Form
